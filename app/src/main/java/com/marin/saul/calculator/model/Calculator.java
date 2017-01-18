296c0ab5a0297c26d7d7cd87785d4032584d1a83
Saul Marin <sau7.mar1n@gmail.com>
2017-01-18 09:17:17 +0100

    Added operation functions and operation switch

--- app/src/main/java/com/marin/saul/calculator/model/Calculator.java
+++ app/src/main/java/com/marin/saul/calculator/model/Calculator.java
@@ -26,6 +26,43 @@ public class Calculator {
     }
 
     public void calculate(int num1, int num2, CalculatorOperation operation){
-
+        int result;
+        switch(operation){
+            case NONE:
+                break;
+            case ADD:
+                result = add(num1,num2);
+                break;
+            case SUBSTRAC:
+                result = substrac(num1,num2);
+                break;
+            case MULTIPLY:
+                result = multiply(num1,num2);
+                break;
+            case DIVIDE:
+                result = divide(num1,num2);
+                break;
+        }
     }
+
+    private int divide(int num1, int num2) {
+        if (num2 == 0){
+            return -1;
+        }
+        return num1/num2;
+    }
+
+    private int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    private int substrac(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    private int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+
 }
