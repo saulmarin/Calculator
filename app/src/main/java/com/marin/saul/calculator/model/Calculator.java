51d4a4ce96d7a2089a138adb94ecbd7cd897626a
Saul Marin <sau7.mar1n@gmail.com>
2017-01-18 08:55:26 +0100

    Calculator.java: created variables and getters/setters

--- app/src/main/java/com/marin/saul/calculator/model/Calculator.java
+++ app/src/main/java/com/marin/saul/calculator/model/Calculator.java
@@ -5,4 +5,23 @@ package com.marin.saul.calculator.model;
  */
 
 public class Calculator {
+
+    public float num1;
+    public float num2;
+
+    public float getNum1() {
+        return num1;
+    }
+
+    public void setNum1(float num1) {
+        this.num1 = num1;
+    }
+
+    public float getNum2() {
+        return num2;
+    }
+
+    public void setNum2(float num2) {
+        this.num2 = num2;
+    }
 }
